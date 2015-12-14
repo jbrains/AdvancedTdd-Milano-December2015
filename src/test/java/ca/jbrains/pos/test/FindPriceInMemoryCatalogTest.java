ff518a95b912db0a03c0734626da2f39c230e9d4
J. B. Rainsberger <me@jbrains.ca>
2015-12-14 17:33:26 +0100

    We have identified tests for the InMemoryCatalog that are really tests for just any Catalog.

--- src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
+++ src/test/java/ca/jbrains/pos/test/FindPriceInMemoryCatalogTest.java
@@ -5,7 +5,6 @@ import ca.jbrains.pos.test.SellOneItemControllerTest.Price;
 import org.junit.Assert;
 import org.junit.Test;
 
-import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -13,20 +12,31 @@ public class FindPriceInMemoryCatalogTest {
     @Test
     public void productFound() throws Exception {
         final Price priceMatchingBarcode = Price.cents(1290);
-        final InMemoryCatalog catalog = new InMemoryCatalog(
-                Collections.singletonMap("91732", priceMatchingBarcode)
-        );
+        final Catalog catalog = createCatalogWith("91732", priceMatchingBarcode);
         Assert.assertEquals(priceMatchingBarcode, catalog.findPrice("91732"));
+    }
+
+    private Catalog createCatalogWith(String barcode, Price priceMatchingBarcode) {
+        return new InMemoryCatalog(
+                new HashMap() {{
+                    put(barcode, priceMatchingBarcode);
+                    put("not " + barcode, Price.cents(-128736));
+                    put("totally not " + barcode, Price.cents(-12763));
+                }}
+        );
     }
 
     @Test
     public void productNotFound() throws Exception {
-        final InMemoryCatalog catalog = new InMemoryCatalog(new HashMap() {{
-            put("not 72624", Price.cents(0));
-            put("still not 72624", Price.cents(0));
-            put("so totally not 72624", Price.cents(0));
+        Assert.assertEquals(null, createCatalogWithout("72624").findPrice("72624"));
+    }
+
+    private Catalog createCatalogWithout(final String barcodeToAvoid) {
+        return new InMemoryCatalog(new HashMap() {{
+            put("not " + barcodeToAvoid, Price.cents(0));
+            put("still not " + barcodeToAvoid, Price.cents(0));
+            put("so totally not " + barcodeToAvoid, Price.cents(0));
         }});
-        Assert.assertEquals(null, catalog.findPrice("72624"));
     }
 
     public static class InMemoryCatalog implements Catalog {
