b483dd8b2b84a86c23e6429adca526cfb9ec67ed
J. B. Rainsberger <me@jbrains.ca>
2015-12-14 09:29:55 +0100

    [FAILING TEST] This thing is on; we can write tests.

---
+++ src/test/java/ca/jbrains/pos/test/IsThisThingOn.java
@@ -0,0 +1,12 @@
+package ca.jbrains.pos.test;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class IsThisThingOn {
+    @Test
+    public void hello() throws Exception {
+
+        Assert.assertEquals(false, true);
+    }
+}
