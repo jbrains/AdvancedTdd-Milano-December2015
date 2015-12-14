0f3d2d1bae2b5c6ec8fad22e7265ac52075df41c
J. B. Rainsberger <me@jbrains.ca>
2015-12-14 17:02:46 +0100

    Our Sell One Item Controller can handle a barcode, no matter what happens.

--- src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
+++ src/test/java/ca/jbrains/pos/test/SellOneItemControllerTest.java
@@ -26,12 +26,30 @@ public class SellOneItemControllerTest {
         controller.onBarcode("12345");
     }
 
+    @Test
+    public void productNotFound() throws Exception {
+        final Catalog catalog = context.mock(Catalog.class);
+        final Display display = context.mock(Display.class);
+
+        context.checking(new Expectations() {{
+            allowing(catalog).findPrice(with("12345"));
+            will(returnValue(null));
+
+            oneOf(display).displayProductNotFoundMessage(with("12345"));
+        }});
+
+        final SellOneItemController controller = new SellOneItemController(catalog, display);
+        controller.onBarcode("12345");
+    }
+
     public interface Catalog {
         Price findPrice(String barcode);
     }
 
     public interface Display {
         void displayPrice(Price price);
+
+        void displayProductNotFoundMessage(String barcodeNotFound);
     }
 
     public static class Price {
@@ -50,7 +68,11 @@ public class SellOneItemControllerTest {
         }
 
         public void onBarcode(String barcode) {
-            display.displayPrice(catalog.findPrice(barcode));
+            final Price price = catalog.findPrice(barcode);
+            if (price == null)
+                display.displayProductNotFoundMessage(barcode);
+            else
+                display.displayPrice(price);
         }
     }
 }
